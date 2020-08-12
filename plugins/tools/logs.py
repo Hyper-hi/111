31914f570765f16a3f72c99010583dccb6ebbd07
Hyper-hi <63490949+Hyper-hi@users.noreply.github.com>
2020-08-12 12:20:15 +0530

    Add files via upload

---
+++ plugins/tools/logs.py
@@ -0,0 +1,44 @@
+# Copyright (C) 2020 by UsergeTeam@Github, < https://github.com/UsergeTeam >.
+#
+# This file is part of < https://github.com/UsergeTeam/Userge > project,
+# and is released under the "GNU v3.0 License Agreement".
+# Please see < https://github.com/uaudith/Userge/blob/master/LICENSE >
+#
+# All rights reserved.
+
+from userge import userge, Message, logging
+
+_LEVELS = {
+    'debug': logging.DEBUG,
+    'info': logging.INFO,
+    'warning': logging.WARNING,
+    'error': logging.ERROR,
+    'critical': logging.CRITICAL
+}
+
+
+@userge.on_cmd("logs", about={'header': "check userge logs"}, allow_channels=False)
+async def check_logs(message: Message):
+    """ check logs """
+    await message.edit("`checking logs ...`")
+    await message.client.send_document(chat_id=message.chat.id,
+                                       document="logs/userge.log",
+                                       caption='userge.log')
+    await message.delete()
+
+
+@userge.on_cmd("setlvl", about={
+    'header': "set logger level, default to info",
+    'types': ['debug', 'info', 'warning', 'error', 'critical'],
+    'usage': "{tr}setlvl [level]",
+    'examples': ["{tr}setlvl info", "{tr}setlvl debug"]})
+async def set_level(message: Message):
+    """ set logger level """
+    await message.edit("`setting logger level ...`")
+    level = message.input_str.lower()
+    if level not in _LEVELS:
+        await message.err("unknown level !")
+        return
+    for logger in (logging.getLogger(name) for name in logging.root.manager.loggerDict):
+        logger.setLevel(_LEVELS[level])
+    await message.edit(f"`successfully set logger level as` : **{level.upper()}**", del_in=3)
